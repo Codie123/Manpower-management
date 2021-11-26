03e084722e746da899edfa3a7c924733d36551ce
elliott6518@gmail.com <User@3601>
2021-11-26 11:05:49 +0530

    Create footer.php
    style : footer of  the website

---
+++ man/footer.php
@@ -0,0 +1,54 @@
+<footer id="footer" class="top-space">
+
+		<div class="footer1">
+			<div class="container">
+				<div class="row">
+					
+					<div class="col-md-3 widget">
+						<h3 class="widget-title">Contact</h3>
+						<div class="widget-body">
+							<p>+123 123456<br>
+								<p>abc@abc.com<br>
+								<p>	Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+								<br>
+								
+							
+						</div>
+					</div>
+
+					<div class="col-md-3 widget">
+						<h3 class="widget-title">Follow Us</h3>
+						<div class="widget-body">
+							<p class="follow-me-icons clearfix">
+								<a href=""><i class="fa fa-facebook fa-2"></i></a>
+								<a href=""><i class="fa fa-instagram fa-2"></i></a>
+								<a href=""><i class="fa fa-twitter fa-2"></i></a>
+							</p>	
+						</div>
+					</div>
+
+					<div class="col-md-6 widget">
+						<h3 class="widget-title">About Us</h3>
+						<div class="widget-body">
+							<p>	Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+						</div>
+					</div>
+
+				</div> <!-- /row of widgets -->
+			</div>
+		</div>
+
+		<div class="footer2">
+			<div class="container">
+				
+<center>		
+	<p style="align-content: center;" >
+								Copyright &copy; Reserved - ManPower Management 2021   
+							</p>
+				
+			
+</center>
+				</div> <!-- /row of widgets -->
+			</div>
+		</div>
+	</footer>	
